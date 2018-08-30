01aecd439e154ed4521601124b376d589b50497c
Nasser Sanou <nassersanou23@gmail.com>
2018-08-30 22:25:28 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -11,7 +11,7 @@ def select_winner(passengers)
   # add the code snippet here!
   passengers.each do |suite, name|
     if name .start_with("A")
-      winner[suite_a] << name 
+      winner["suite_a"] += "#{name}
     end 
 end 
 end
